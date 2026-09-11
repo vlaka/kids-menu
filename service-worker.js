af74c06087b8ff29ed5fdff5b60d18b25a16ec8f
Vlad Khudyakov <vlad.khudyakov@gmail.com>
2026-09-12 01:09:14 +0300

    Cache recreated AVIF dish photos

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kids-menu-v22';
+const CACHE_NAME = 'kids-menu-v23';
 const APP_SHELL = [
   './',
   './index.html',
@@ -9,8 +9,8 @@ const APP_SHELL = [
   './manifest.webmanifest?v=18',
   './data/menu.json',
   './assets/schnitzel-icon.svg',
-  './assets/dishes/banana-20260912-0055.jpg',
-  './assets/dishes/black-bread-20260912-0055.jpg'
+  './assets/dishes/banana-20260912-0115.avif',
+  './assets/dishes/black-bread-20260912-0115.avif'
 ];
 
 self.addEventListener('install', event => {
